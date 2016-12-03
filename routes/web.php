80c1b5fb92c4928fa985f211f9fe93447ab51c43
Matthew Hatcher <hello@matthewh.in>
2016-12-03 01:41:15 -0500

    Adding base routes.

--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,32 @@ Route::group([
 	'prefix' => 'developer',
 	'as' => 'developer::'
 ], function() {
-	Route::get('login', ['as' => 'auth.login', 'uses' => 'Auth\AuthController@showLoginForm']);
-	Route::post('login', ['as' => 'auth.login', 'uses' => 'Auth\AuthController@login']);
-	Route::get('logout', ['as' => 'auth.logout', 'uses' => 'Auth\AuthController@logout']);
+	Route::get('login', ['as' => 'auth.login', 'uses' => 'Auth\LoginController@showLoginForm']);
+	Route::post('login', ['as' => 'auth.login', 'uses' => 'Auth\LoginController@login']);
+	Route::get('logout', ['as' => 'auth.logout', 'uses' => 'Auth\LoginController@logout']);
 
 	// Registration Routes...
-	Route::get('register', ['as' => 'auth.register', 'uses' => 'Auth\AuthController@showRegistrationForm']);
-	Route::post('register', ['as' => 'auth.register', 'uses' => 'Auth\AuthController@register']);
+	Route::get('register', ['as' => 'auth.register', 'uses' => 'Auth\RegisterController@showRegistrationForm']);
+	Route::post('register', ['as' => 'auth.register', 'uses' => 'Auth\RegisterController@register']);
 
 	// Password Reset Routes...
-	Route::get('password/reset/{token?}', ['as' => 'auth.password.reset', 'uses' => 'Auth\PasswordController@showResetForm']);
-	Route::post('password/email', ['as' => 'auth.password.email', 'uses' => 'Auth\PasswordController@sendResetLinkEmail']);
-	Route::post('password/reset', ['as' => 'auth.password.reset', 'uses' => 'Auth\PasswordController@reset']);
+	Route::get('password/reset/{token?}', ['as' => 'auth.password.reset', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
+	Route::post('password/email', ['as' => 'auth.password.email', 'uses' => 'Auth\ResetPasswordController@sendResetLinkEmail']);
+	Route::post('password/reset', ['as' => 'auth.password.reset', 'uses' => 'Auth\ResetPasswordController@reset']);
+
+	Route::group([
+		'prefix' => 'home',
+		'as' => 'home::'
+		'middleware' => ['role:developer|admin']
+	], function() {
+		Route::get('/', []);
+	});
+
+	Route::group([
+		'prefix' => 'admin',
+		'as' => 'admin::',
+		'middleware' => ['role:admin']
+	], function() {
+		Route::get('/', []);
+	});
 });
-
-//Auth::routes();
-
-//Route::get('/home', 'HomeController@index');
