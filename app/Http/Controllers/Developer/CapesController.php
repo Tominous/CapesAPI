6530007756a84f65984829e737fe918fbd364f97
Matthew Hatcher <matthewhatcher1@gmail.com>
2016-12-06 23:59:34 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Developer/CapesController.php
+++ app/Http/Controllers/Developer/CapesController.php
@@ -2,113 +2,121 @@
 
 namespace CapesAPI\Http\Controllers\Developer;
 
+use Auth;
+use Capes;
 use CapesAPI\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Projects;
-use Capes;
-use Validator;
-use Auth;
 use Storage;
+use Validator;
 
 class CapesController extends Controller
 {
-	public function getCapes($hash) {
-		$project = Projects::where('hash', $hash)->first();
-		$capes = Capes::where('project_id', $project->id)->paginate();
+    public function getCapes($hash)
+    {
+        $project = Projects::where('hash', $hash)->first();
+        $capes = Capes::where('project_id', $project->id)->paginate();
 
-		return view('developer.project.capes.capes', ['project' => $project, 'capes' => $capes]);
-	}
+        return view('developer.project.capes.capes', ['project' => $project, 'capes' => $capes]);
+    }
 
-	public function deleteCape($hash, $capeHash) {
-		$project = Projects::where('hash', $hash)->first();
+    public function deleteCape($hash, $capeHash)
+    {
+        $project = Projects::where('hash', $hash)->first();
 
-		if($project->developer_id != Auth::user()->id)
-			abort(403);
+        if ($project->developer_id != Auth::user()->id) {
+            abort(403);
+        }
 
-		$cape = Capes::where([
-			'hash' => $capeHash,
-			'project_id' => $project->id
-		])->first();
+        $cape = Capes::where([
+            'hash'       => $capeHash,
+            'project_id' => $project->id,
+        ])->first();
 
-		$dir = 'public/' . Auth::user()->email . '/' . $hash . '/' . $capeHash;
-		Storage::deleteDirectory($dir);
-		$cape->delete();
+        $dir = 'public/'.Auth::user()->email.'/'.$hash.'/'.$capeHash;
+        Storage::deleteDirectory($dir);
+        $cape->delete();
 
-		return redirect()->route('developer::project::capes', ['hash' => $hash]);
-	}
+        return redirect()->route('developer::project::capes', ['hash' => $hash]);
+    }
 
-	public  function showCreateCape($hash) {
-		$project = Projects::where('hash', $hash)->first();
+    public function showCreateCape($hash)
+    {
+        $project = Projects::where('hash', $hash)->first();
 
-		return view('developer.project.capes.create', ['project' => $project, 'hash' => $hash]);
-	}
+        return view('developer.project.capes.create', ['project' => $project, 'hash' => $hash]);
+    }
 
-	public function createCape(Request $request, $hash) {
-		$rules = [
-			'name' => 'required|max:255|min:4',
-			'cape-template' => 'required|image|mimes:png,jpeg|max:3072'
-		];
+    public function createCape(Request $request, $hash)
+    {
+        $rules = [
+            'name'          => 'required|max:255|min:4',
+            'cape-template' => 'required|image|mimes:png,jpeg|max:3072',
+        ];
 
-		$validation = Validator::make($request->all(), $rules);
+        $validation = Validator::make($request->all(), $rules);
 
-		if($validation->fails()) {
-			return redirect()->back()->withInput()->withErrors($validation);
-		} else {
-			$capeHash = '';
+        if ($validation->fails()) {
+            return redirect()->back()->withInput()->withErrors($validation);
+        } else {
+            $capeHash = '';
 
-			do {
-				$capeHash = str_random(6);
-				$capeHash_db = Capes::where('hash', $capeHash)->first();
-			} while(!empty($capeHash_db));
+            do {
+                $capeHash = str_random(6);
+                $capeHash_db = Capes::where('hash', $capeHash)->first();
+            } while (!empty($capeHash_db));
 
-			Capes::create([
-				'project_id' => Auth::user()->id,
-				'hash' => $capeHash,
-				'name' => ($request->has('name') ? $request->get('name') : 'An Awesome Cape'),
-			]);
+            Capes::create([
+                'project_id' => Auth::user()->id,
+                'hash'       => $capeHash,
+                'name'       => ($request->has('name') ? $request->get('name') : 'An Awesome Cape'),
+            ]);
 
-			$request->file('cape-template')->storeAs(
-				'public/' . Auth::user()->email . '/' . $hash . '/' . $capeHash,
-				'cape.png'
-			);
-		}
+            $request->file('cape-template')->storeAs(
+                'public/'.Auth::user()->email.'/'.$hash.'/'.$capeHash,
+                'cape.png'
+            );
+        }
 
-		return redirect()->route('developer::project::capes', ['hash' => $hash]);
-	}
+        return redirect()->route('developer::project::capes', ['hash' => $hash]);
+    }
 
-	public function editCape(Request $request, $hash, $capeHash) {
-		$project = Projects::where('hash', $hash)->first();
+    public function editCape(Request $request, $hash, $capeHash)
+    {
+        $project = Projects::where('hash', $hash)->first();
 
-		if($project->developer_id != Auth::user()->id)
-			abort(403);
+        if ($project->developer_id != Auth::user()->id) {
+            abort(403);
+        }
 
-		$cape = Capes::where([
-			'hash' => $capeHash,
-			'project_id' => $project->id
-		])->first();
+        $cape = Capes::where([
+            'hash'       => $capeHash,
+            'project_id' => $project->id,
+        ])->first();
 
-		$rules = [
-			'name' => 'max:255|min:4',
-			'cape-template' => 'image|mimes:png,jpeg|max:3072'
-		];
+        $rules = [
+            'name'          => 'max:255|min:4',
+            'cape-template' => 'image|mimes:png,jpeg|max:3072',
+        ];
 
-		$validation = Validator::make($request->all(), $rules);
+        $validation = Validator::make($request->all(), $rules);
 
-		if($validation->fails())
-			return redirect()->back()->withInput()->withErrors($validation);
-		
-		if($request->has('name')) {
-			$cape->name = $request->get('name');
-			$cape->save();
-		}
+        if ($validation->fails()) {
+            return redirect()->back()->withInput()->withErrors($validation);
+        }
 
-		if($request->hasFile('cape-template')) {
-			$request->file('cape-template')->storeAs(
-				'public/' . Auth::user()->email . '/' . $hash . '/' . $capeHash,
-				'cape.png'
-			);
-		}
+        if ($request->has('name')) {
+            $cape->name = $request->get('name');
+            $cape->save();
+        }
 
-		return redirect()->route('developer::project::capes', ['hash' => $hash]);
-	} 
+        if ($request->hasFile('cape-template')) {
+            $request->file('cape-template')->storeAs(
+                'public/'.Auth::user()->email.'/'.$hash.'/'.$capeHash,
+                'cape.png'
+            );
+        }
+
+        return redirect()->route('developer::project::capes', ['hash' => $hash]);
+    }
 }
--- app/Http/Middleware/CheckUserRole.php
+++ app/Http/Middleware/CheckUserRole.php
@@ -3,25 +3,27 @@
 namespace CapesAPI\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
 
 class CheckUserRole
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if($request->user()) {
-            if($request->user()->hasRole('banned') && $request->path() !== 'banned')
+        if ($request->user()) {
+            if ($request->user()->hasRole('banned') && $request->path() !== 'banned') {
                 return redirect()->route('banned');
+            }
 
-            if($request->user()->hasRole('unverified') && $request->path() !== 'unverified')
+            if ($request->user()->hasRole('unverified') && $request->path() !== 'unverified') {
                 return redirect()->route('unverified');
+            }
         }
 
         return $next($request);
