db0aa4d6cff25973385973625b8c81b65b9bee93
Matthew Hatcher <hello@matthewh.in>
2016-12-07 03:58:39 -0500

    Disabling button on click.

--- resources/views/developer/project/capes/create.blade.php
+++ resources/views/developer/project/capes/create.blade.php
@@ -51,7 +51,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" id="createCapeButton" class="btn btn-primary" onclick="document.getElementById('createCapeButton').setAttribute('disabled', 'disabled')">
                                     Create Cape
                                 </button>
                             </div>
