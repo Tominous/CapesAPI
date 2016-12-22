866b92ab765bff5756c82717066ce792a0d73ac3
Matthew Hatcher <MatthewSH@users.noreply.github.com>
2016-12-22 03:29:25 -0500

    Create ad.blade.php

---
+++ resources/views/includes/ad.blade.php
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Capes API -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6691059885236141"
+     data-ad-slot="7653117576"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
