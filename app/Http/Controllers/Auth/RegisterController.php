b422f5f43ee49df2b76ae8c45d971c1fa1d91ab3
Matthew Hatcher <MatthewSH@users.noreply.github.com>
2016-12-22 13:01:23 -0500

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name'     => 'required|max:255',
+            'name'     => 'required|between:5,255|unique:users|alpha_dash',
             'email'    => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
