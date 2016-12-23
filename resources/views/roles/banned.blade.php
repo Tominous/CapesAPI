210d8a5f82b64b6045f818ae866e3f90ce76e405
Matthew Hatcher <MatthewSH@users.noreply.github.com>
2016-12-22 22:34:08 -0500

    Create banned.blade.php

---
+++ resources/views/roles/banned.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Account Banned.</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato', sans-serif;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">
+                  You've been banned.
+                  <br/><br/>
+                  If you believe this banning was unfair, please make a ticket 
+                  <a href="https://github.com/halfpetal/Support/issues" target="_blank">here</a>. Thank you.
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
