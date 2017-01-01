b7dc358a13c6e2101c3a01aeee7a1987bb01edee
Matthew Hatcher <matthewhatcher1@gmail.com>
2017-01-01 18:21:14 -0500

    Creating code login model and migration.

---
+++ app/MojangLoginCode.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CapesAPI;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MojangLoginCode extends Model
+{
+    //
+}
---
+++ database/migrations/2017_01_01_231647_create_mojang_login_codes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMojangLoginCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mojang_login_codes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('uuid');
+            $table->string('code')->unique();
+            $table->boolean('used')->defaults(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mojang_login_codes');
+    }
+}
