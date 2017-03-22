3168aca8a1f0dee095eb70bb8957536ace8e87ad
Matthew Hatcher <matthewhatcher1@gmail.com>
2017-03-21 21:45:42 -0400

    Removing ad.

--- app/Http/Controllers/API/v1/UserController.php
+++ app/Http/Controllers/API/v1/UserController.php
@@ -21,12 +21,6 @@ class UserController extends Controller
         ])->first();
 
         if ($activeCape === null) {
-            $showAd = random_int(1, 100);
-
-            if ($showAd <= 25) {
-                return redirect('http://a.targetserving.com/request/?z=410343');
-            }
-
             abort(404);
         }
 
